886fe6a4784529c815776acb8386a4ae9d4b16fa
create-xiaoyu <abc18873060306@qq.com>
2025-11-01 01:05:50 +0800

    添加kubejs脚本I18n本地化key和文本

---
+++ kubejs/client_scripts/Language/en_us.js
@@ -0,0 +1,10 @@
+// priority: 500
+
+ClientEvents.lang("en_us", (event) => {
+    event.add("greedycraft.modepack.name", "§bGreedyCraft§7: §c§lNIRVANA")
+    event.add("greedycraft.commands.setpackmode", "§6Pack mode has been changed to: ")
+    event.add("greedycraft.commands.error.permissions", "§cYou do not have permission to execute this command!")
+    event.add("greedycraft.packmode.casual", "§aCasual")
+    event.add("greedycraft.packmode.adventure", "§eAdventure")
+    event.add("greedycraft.packmode.expert", "§c§lExpert")
+})
---
+++ kubejs/client_scripts/Language/zh_cn.js
@@ -0,0 +1,10 @@
+// priority: 500
+
+ClientEvents.lang("zh_cn", (event) => {
+    event.add("greedycraft.modepack.name", "§b贪婪整合包§7：§c§l涅槃")
+    event.add("greedycraft.commands.setpackmode", "§6已修改整合包模式为：")
+    event.add("greedycraft.commands.error.permissions", "§c你没有权限执行该命令！")
+    event.add("greedycraft.packmode.casual", "§a休闲")
+    event.add("greedycraft.packmode.adventure", "§e冒险")
+    event.add("greedycraft.packmode.expert", "§c§l专家")
+})
