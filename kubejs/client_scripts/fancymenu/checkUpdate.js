3c78f7039da3b10540b4308db4936c69bc416c42
create-xiaoyu <abc18873060306@qq.com>
2025-11-18 20:51:59 +0800

    修复版本检查

--- kubejs/client_scripts/fancymenu/checkUpdate.js
+++ kubejs/client_scripts/fancymenu/checkUpdate.js
@@ -4,7 +4,7 @@ if (global.checkVersionUpdate == "true") {
     let newVersionCode = KJSutils.Analysis("config/greedycraft/version.json", "$.versionCode")
     let newVersionName = KJSutils.Analysis("config/greedycraft/version.json", "$.versionName")
 
-    if (global.localVersionCode < newVersionCode) {
+    if (global.localPackVersionCode < newVersionCode) {
         KJSutils.FMsetVariable("packUpdate", "true")
         KJSutils.FMsetVariable("packNewVersion", newVersionName)
         KJSutils.FMsetVariable("packNewVersionNumber", newVersionCode - global.localPackVersionCode)
