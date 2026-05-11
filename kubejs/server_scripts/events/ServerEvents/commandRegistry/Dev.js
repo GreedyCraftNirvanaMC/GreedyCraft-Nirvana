06070d821dfdda6cb6d64013e6ee6e9eae9b2de8
create-xiaoyu <abc18873060306@qq.com>
2026-05-11 18:40:34 +0800

    添加开发用命令

---
+++ kubejs/server_scripts/events/ServerEvents/commandRegistry/Dev.js
@@ -0,0 +1,19 @@
+// 服务器事件-命令注册事件
+// 此脚本用于注册整合包开发用命令 gctdev
+// priority: 50
+
+ServerEvents.commandRegistry(event => {
+    const { commands: Commands, arguments: Arguments } = event
+    event.register(
+        Commands.literal("gctdev")
+            .then(
+                Commands.literal("showmodlist").requires(source => global.VARIABLE_CREATOR_LIST.includes(source.player.uuid.toString()))
+                    .executes(ctx => {
+                        Platform.mods.forEach((MOD_ID, MOD_INFO) => {
+                            console.log('"' + MOD_ID + '"')
+                        })
+                        return 1
+                    })
+            )
+    )
+})
