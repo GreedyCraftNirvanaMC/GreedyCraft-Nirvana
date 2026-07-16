cee3680608cefa071d17a859ff9165fc76f9957d
create-xiaoyu <abc18873060306@qq.com>
2026-07-16 10:03:52 +0800

    修改海洋生物群系海底方块破坏放置水逻辑

--- kubejs/server_scripts/events/BlockEvents/broken/OceanBiomeSetWater.js
+++ kubejs/server_scripts/events/BlockEvents/broken/OceanBiomeSetWater.js
@@ -6,10 +6,21 @@ BlockEvents.broken(event => {
     let level = event.getLevel()
     let block = event.getBlock()
     let biome = level.getBiome(block.getPos())
+    let pos = block.getPos()
 
     // 判断是不是玩家破坏的 && 判断被破坏方块的 Y 坐标是否小于 40 && 判断是不是主世界 && 判断生物群系 Tag 是否包含 is_ocean
-    if (event.getEntity().isPlayer() && block.getY() < 40 && level.isOverworld() && biome.isTag("c:is_ocean")) {
-        // 设置方块为水
-        level.setBlock(block.getPos(), "minecraft:water", 3)
+    if (block.getY() < 40 && level.isOverworld() && biome.isTag("c:is_ocean")) {
+        // 判断被破坏方块的六面是否接触到水
+        if (
+            level.getBlock(pos.getX() + 1, pos.getY(), pos.getZ()).getId() == "minecraft:water" ||
+            level.getBlock(pos.getX() - 1, pos.getY(), pos.getZ()).getId() == "minecraft:water" ||
+            level.getBlock(pos.getX(), pos.getY() + 1, pos.getZ()).getId() == "minecraft:water" ||
+            level.getBlock(pos.getX(), pos.getY() - 1, pos.getZ()).getId() == "minecraft:water" ||
+            level.getBlock(pos.getX(), pos.getY(), pos.getZ() + 1).getId() == "minecraft:water" ||
+            level.getBlock(pos.getX(), pos.getY(), pos.getZ() - 1).getId() == "minecraft:water"
+        ) {
+            // 放置水
+            level.setBlock(block.getPos(), "minecraft:water", 3)
+        }
     }
 })
