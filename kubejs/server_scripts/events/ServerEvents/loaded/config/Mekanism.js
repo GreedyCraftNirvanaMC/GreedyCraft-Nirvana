f467b58667fcf4d7c273e0c736d5bd4245e0718f
create-xiaoyu <abc18873060306@qq.com>
2026-07-20 17:19:49 +0800

    修复脚本报错

---
+++ kubejs/server_scripts/events/ServerEvents/loaded/config/Mekanism.js
@@ -0,0 +1,13 @@
+// priority: 50
+
+let pack_mode = KJSutils.Analysis("config/greedycraft/config.json", "$.packMode").toUpperCase()
+
+ServerEvents.loaded(event => {
+    let $MekanismConfig = Java.loadClass("mekanism.common.config.MekanismConfig")
+
+    /** @type {number} */
+    let TempMultiplierValue = global[`VARIABLE_${pack_mode}_MEKANISM_BASEVALUE`].TEPValue
+
+    $MekanismConfig.general.evaporationTempMultiplier.set(TempMultiplierValue)
+    $MekanismConfig.general.save()
+})
