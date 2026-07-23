2daf9a8bd88e3f796db6c18a0d1d747383c9d19b
create-xiaoyu <abc18873060306@qq.com>
2026-07-23 22:17:11 +0800

    适应KJSUtils新版本事件

--- kubejs/server_scripts/events/KJSUtilsEvents/playerEnteredDimension/ChangeDifficulty.js
+++ kubejs/server_scripts/events/KJSUtilsEvents/playerEnteredDimension/ChangeDifficulty.js
@@ -2,16 +2,16 @@
 // 此脚本用于在玩家进入指定维度时增加对应难度
 // priority: 50
 
-KJSutilEvents.playerLoggedInDimension(event => {
+KJSUtilsEvents.playerEnteredDimension(event => {
     let player = event.getPlayer()
     let server = player.getServer()
     let playerName = player.getUsername()
-    let loggedInDimension = event.getLoggedIn()
+    let enteredDimension = event.getTo().location()
 
     // 获取维度与难度的对应关系
     Object.entries(global.MAP_DIMENSION_DIFFICULTY).forEach(([dimension, difficulty]) => {
         // 判断进入的哪个维度
-        if (loggedInDimension == dimension) {
+        if (enteredDimension == dimension) {
             // 增加难度
             server.runCommandSilent(`ps_difficulty add ${playerName} ${difficulty}`)
         }
--- kubejs/server_scripts/events/KJSUtilsEvents/playerLeftDimension/ChangeDifficulty.js
+++ kubejs/server_scripts/events/KJSUtilsEvents/playerLeftDimension/ChangeDifficulty.js
@@ -2,16 +2,16 @@
 // 此脚本用于在玩家退出指定维度时减少对应难度
 // priority: 50
 
-KJSutilEvents.playerLoggedOutDimension(event => {
+KJSUtilsEvents.playerLeftDimension(event => {
     let player = event.getPlayer()
     let server = player.getServer()
     let playerName = player.getUsername()
-    let loggedOutDimension = event.getLoggedOut()
+    let leftDimension = event.getFrom().location()
 
     // 获取维度与难度的对应关系
     Object.entries(global.MAP_DIMENSION_DIFFICULTY).forEach(([dimension, difficulty]) => {
         // 判断进入的哪个维度
-        if (loggedOutDimension == dimension) {
+        if (leftDimension == dimension) {
             // 增加难度，但是难度为负数，所以就变成减了
             server.runCommandSilent(`ps_difficulty add ${playerName} -${difficulty}`)
         }
