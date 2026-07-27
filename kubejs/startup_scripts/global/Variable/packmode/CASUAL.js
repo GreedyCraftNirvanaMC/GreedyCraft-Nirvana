2d2fa086a8ff5134db771bedf9e1b0630fd03c49
create-xiaoyu <abc18873060306@qq.com>
2026-07-27 12:00:55 +0800

    漏了一个

--- kubejs/startup_scripts/global/Variable/packmode/CASUAL.js
+++ kubejs/startup_scripts/global/Variable/packmode/CASUAL.js
@@ -1,6 +1,9 @@
 // priority: 32767
 
-global.VARIABLE_CASUAL_MEKANISM_BASEVALUE = {
+global.VARIABLE_ADVENTURE_COMMON_BASEVALUE = {
     "Energy": 0.5,
+}
+
+global.VARIABLE_CASUAL_MEKANISM_BASEVALUE = {
     "TEPValue": 3.7
 }
