b74b0569679ea822e16f8511b5d909b2fc2ab363
create-xiaoyu <abc18873060306@qq.com>
2026-07-27 11:34:58 +0800

    注册氯化钠与氯化钠晶体配方

--- kubejs/server_scripts/events/ServerEvents/recipes/Mekanism/brine.js
+++ kubejs/server_scripts/events/ServerEvents/recipes/Mekanism/brine.js
@@ -2,6 +2,29 @@
 // 此脚本用于注册氯化钠生产盐水配方
 // priority: 50
 
-// ServerEvents.recipes(event => {
-//     event.recipes.mekanism.chemical_infusing("mekanism:", leftInput, rightInput)
-// })
+ServerEvents.recipes(event => {
+    event.recipes.mekanism.chemical_infusing("10000x greedycraft:sodium_chloride_chemical", "10x mekanism:chlorine", "10x mekanism:sodium")
+    event.custom(
+        {
+            "type": "mekanism:reaction",
+            "chemical_input": {
+                "amount": 1000,
+                "chemical": "greedycraft:sodium_chloride_chemical"
+            },
+            "duration": 200,
+            "energy_required": 1000,
+            "fluid_input": {
+                "amount": 100,
+                "tag": "minecraft:water"
+            },
+            "item_input": {
+                "count": 1,
+                "item": "aether:ambrosium_shard"
+            },
+            "item_output": {
+                "count": 10,
+                "id": "greedycraft:sodium_chloride_crystal"
+            }
+        }
+    )
+})
